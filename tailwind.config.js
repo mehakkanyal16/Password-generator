5ffbc5fb520dad0cced07a8e1311159f0f9c480f
mehak <mehakkanyal@gmail.com>
2025-02-25 22:14:22 +0530

    Added new folder with files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"], // Ensure your files are included
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

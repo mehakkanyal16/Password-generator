75e5c9fcc79fdff98666ae42cb6090def78fded7
mehak <mehakkanyal@gmail.com>
2025-02-25 22:15:29 +0530

    Deleted all files

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"], // Ensure your files are included
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
